eff341300139b1464167dcc7b3b04fa0525436aa
fdlii <155849941+fdlii@users.noreply.github.com>
2024-03-12 20:47:58 +0300

    Update alg4.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,42 @@
-// Copyright 2021 NNTU-CS
+int bsearch(int* arr, int len, int value) {
+    if (len == 0) return 0;
+    if (arr[len / 2] == value) return value;
+    if (arr[len / 2] > value) return bsearch(arr, len / 2, value);
+    if (arr[len / 2] < value) return bsearch(arr + len / 2, len / 2, value);
+}
+
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    sort(arr, arr+len);
+    int count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) count++;
+        }
+    }
+    return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    sort(arr, arr + len);
+    int count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = len - 1; j > i; j--) {
+            if (arr[i] + arr[j] == value) count++;
+            if (arr[i] + arr[j] > value) {
+                len--;
+            }
+            if (arr[i] + arr[j] < value) {
+                break;
+            }
+        }
+    }
+    return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    sort(arr, arr + len);
+    int x, count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        x = value - arr[i];
+        if (x == bsearch(arr + i + 1, len - 1 - i, x)) count++;
+    }
+    return count+1;
 }
